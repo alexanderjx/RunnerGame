162dfacfd8fe3a5f4056e6712124866b5f99cba0
Alexander Juxley <Kozak0432@outlook.com>
2024-11-13 13:32:19 -0500

    Empty Game Class

---
+++ RunnerGame/RunGame.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunnerGame
+{
+    internal class RunGame
+    {
+        bool isPlaying = false;
+        int gDiff = 1;
+
+    }
+}
